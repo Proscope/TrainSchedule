fa81041f4c860219d97694b589a0793f1539e3e7
Harrison McCord <harrison.mccord@gmail.com>
2017-06-10 12:34:26 -0400

    inti commit

---
+++ assets/javascript/train.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAt4v_1kF0eTuFyGY9BIKJmqTYzOwY8AXY",
+    authDomain: "trainschedule-387f8.firebaseapp.com",
+    databaseURL: "https://trainschedule-387f8.firebaseio.com",
+    projectId: "trainschedule-387f8",
+    storageBucket: "trainschedule-387f8.appspot.com",
+    messagingSenderId: "741028564328"
+  };
+  firebase.initializeApp(config);
