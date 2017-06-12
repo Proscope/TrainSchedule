28044a84d2c29481699f09ff2b1827252063bdc1
Harrison McCord <harrison.mccord@gmail.com>
2017-06-12 12:01:51 -0400

    html built

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -8,3 +8,35 @@
     messagingSenderId: "741028564328"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#add-train-btn").on("click", function(event) {
+  	event.preventDefault();
+
+  	var trnName = $("#train-name-input").val().trim();
+  	var trnDestination = $("#destination-input").val().trim();
+  	var trnFirstTime = moment($("#first-train-time-input").val().trim(), "HH:mm").format("X");
+  	var trnFrequency = $("#frequency-input").val().trim();
+
+  	var newTrain = {
+  		name: trnName,
+  		destination: trnDestination,
+  		first: trnFirstTime,
+  		frequency: trnFrequency
+  	};
+
+  	database.ref().push(newTrain);
+
+  	console.log(newTrain.name);
+  	console.log(newTrain.destination);
+  	console.log(newTrain.first);
+  	console.log(newTrain.frequency);
+
+  	alert("New Train Successfully Added");
+
+  	$("#train-name-input").val("");
+  	$("#destination-input").val("");
+  	$("#first-train-time-input-input").val("");
+  	$("#frequency-input").val("");
+  });
